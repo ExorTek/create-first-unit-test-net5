9203974cc8f7587aac60a23b6680ac24d85f8940
Mehmet D <exortek979@gmail.com>
2021-08-13 11:26:31 +0300

    -

---
+++ ShoppingCart.Test/CartTest.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ShoppingCart.Test
+{
+    [TestClass]
+    public class CartTest
+    {
+        CartItem _cartItem;
+        CartManager _cartManager;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            //Arrange
+            _cartManager = new CartManager();
+            _cartItem = new CartItem
+            {
+                Product = new Product()
+                {
+                    ProductId = 1,
+                    ProductName = "Laptop",
+                    UnitPrice = 1500
+                },
+                Quantity = 1
+            };
+            _cartManager.Add(_cartItem);
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            _cartManager.Clear();
+        }
+
+        [TestMethod]
+        public void AddToCart()
+        {
+            const int expectedProduct = 1;
+
+            //Act
+            var totalItem = _cartManager.TotalItem;
+
+            //Assert
+            Assert.AreEqual(expectedProduct, totalItem);
+        }
+
+        [TestMethod]
+        public void RemoveToCartProduct()
+        {
+            var sumCartProduct = _cartManager.TotalItem;
+            //Act
+            _cartManager.Remove(1);
+            var lastCartProduct = _cartManager.TotalItem;
+
+            //Assert
+            Assert.AreEqual(sumCartProduct - 1, lastCartProduct);
+        }
+
+        [TestMethod]
+        public void RemoveToCart()
+        {
+            //Act
+            _cartManager.Clear();
+            //Assert
+            Assert.AreEqual(0, _cartManager.TotalItem);
+            Assert.AreEqual(0, _cartManager.TotalQuantity);
+        }
+    }
+}
